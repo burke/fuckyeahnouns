66ba329a2a92558495913e5e65668a11f5b2045b
Burke Libbey <burke@burkelibbey.org>
2011-03-11 00:12:30 -0600

    blacklist

--- fyn.rb
+++ fyn.rb
@@ -69,7 +69,7 @@ module FuckYeahNouns
       redirect "http://www.cafepress.com/fuckyeahnouns.#{pid}"
     end 
 
-    BLACKLIST = ["selinaferguson", "pwaring",'eddsowden','shakarshy']
+    BLACKLIST = ["selinaferguson", "pwaring",'eddsowden','shakarshy','nickbrom']
     
     get '/images/:noun' do
       idx = params[:idx] || 0
