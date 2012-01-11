6970352a23bef0eda9bc4548e518baf64a4300e3
Stefan Penner <stefan.penner@gmail.com>
2012-01-10 21:21:16 -0600

    #fetch has moved

--- actions/image.rb
+++ actions/image.rb
@@ -1,5 +1,16 @@
 module Actions
   class Image
+    module Didntfindshit
+
+      def file
+        './didntfindshit.jpg'
+      end
+
+      def max_age
+        30
+      end
+    end
+
     attr_accessor :file, :max_age
 
     def initialize(noun)
@@ -9,8 +20,8 @@ module Actions
     end
 
     def fetch
-      @file = FuckYeahNouns.fetch_image(noun)
-      FuckYeahNouns.annotate(@file, noun, shirtastic)
+      @file = Image.fetch_image(noun)
+      Image.annotate(@file, noun, shirtastic)
     end
 
     def self.create(noun)
@@ -24,16 +35,85 @@ module Actions
 
       instance
     end
-  end
 
-  module Didntfindshit
+    def self.fetch_image(noun, idx=0)
+      url = "http://boss.yahooapis.com/ysearch/images/v1/#{CGI.escape noun}?appid=#{ENV['APP_ID']}"
+      # url = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=#{CGI.escape noun}"
 
-    def file
-      './didntfindshit.jpg'
+
+      # seriously, seriously need to rewrite this clusterfuck. What am I thinking?
+      # It's 2:30am. That's my excuse.
+      retries = 1
+      begin
+        res = nil
+        Timeout::timeout(4) do
+          res = JSON.parse(open(url).read)
+        end
+      rescue Timeout::Error
+        retries -= 1
+        if retries >= 0
+          retry
+        else
+          raise "omg"
+        end
+      end
+
+      set = res['ysearchresponse']['resultset_images']
+      raise if set.size.zero?
+      begin
+        r = nil
+        Timeout::timeout(4) do
+          r=open(set[0]['url'])
+        end
+        r
+      rescue StandardError, Timeout::Error
+        begin
+          r = nil
+          Timeout::timeout(4) do
+            r=open(set[1]['url'])
+          end
+          r
+        rescue Timeout::Error
+          raise "omg"
+        end
+      end
     end
 
-    def max_age
-      30
+    def self.annotate(img, noun, shirtastic=false)
+      picture = Magick::Image.from_blob(img.read).first
+      width,height = picture.columns, picture.rows
+
+      if shirtastic
+        factor = 2000/600.0
+        if width > height
+          picture.resize!(2000,2000*(height/width.to_f))
+        else
+          picture.resize!(2000*(width/height.to_f), 2000)
+        end
+      else
+        factor = 1
+        picture.resize!(600,600*(height/width.to_f))
+      end
+      width,height = picture.columns, picture.rows
+
+      overlay = Magick::Image.new(width, 100 * factor)
+      picture.composite!(overlay, Magick::SouthGravity, Magick::MultiplyCompositeOp)
+
+      caption = Magick::Draw.new
+      caption.fill('white')
+      caption.stroke('black')
+      caption.font_stretch = Magick::ExtraCondensedStretch
+      caption.font('Helvetica-Bold')
+      caption.stroke_width(2 * factor)
+      caption.pointsize(48 * factor)
+      caption.font_weight(800)
+      caption.text_align(Magick::CenterAlign)
+
+      caption.text(width/2.0, height-(50*factor), "FUCK YEAH\n#{noun.upcase}")
+      caption.draw(picture)
+
+      return picture.to_blob
     end
   end
+
 end
