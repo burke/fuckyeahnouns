ddee6427cd77e30229554370058fd2dc934da869
Stefan Penner <stefan.penner@gmail.com>
2012-01-10 21:50:17 -0600

    test NSFW + refactor

--- spec/actions/noun_spec.rb
+++ spec/actions/noun_spec.rb
@@ -3,6 +3,18 @@ require './boot'
 require './actions/noun'
 
 describe Actions::Noun do
+  describe "valid noun" do
+    let(:subject) do
+      Actions::Noun.create("sleepy")
+    end
+
+    it "runs" do
+      subject.should_not be_nil
+    end
+  end
+end
+
+describe Actions::Noun::BlackListed do
   describe '.blacklisted?' do
     let(:subject) { Actions::Noun::BlackListed }
     context 'blacklisted' do
@@ -17,14 +29,21 @@ describe Actions::Noun do
       end
     end
   end
+end
 
-  describe "valid noun" do
-    let(:subject) do
-      Actions::Noun.create("sleepy")
+describe Actions::Noun::NSFW do
+  describe '.nsfw?' do
+    let(:subject) { Actions::Noun::NSFW }
+    context 'is nsfw' do
+      it 'is true' do
+        subject.nsfw?('boob').should be_true
+      end
     end
 
-    it "runs" do
-      subject.should_not be_nil
+    context 'is not nsfw' do
+      it 'is false' do
+        subject.nsfw?('sleepy').should be_false
+      end
     end
   end
 end
