a7546b8bcade05f11c076af04adec952f16803c2
Stefan Penner <stefan.penner@gmail.com>
2012-01-10 21:20:23 -0600

    small refactor

--- actions/noun.rb
+++ actions/noun.rb
@@ -1,8 +1,30 @@
 module Actions
   class Noun
-    BLACKLIST = ["selinaferguson", "pwaring",'eddsowden','shakarshy','nickbrom', 'julietuesley','andrewbrin','dtox','abigailwessel', 'abby', 'angelaparriott', 'elizabethparriott']
-    WORK_INAPPROPRIATE = /boob|tit|cock|penis|vagina|pussy|dick|ass|fuck|shit|piss|sex|gay|lesbian|chick/
     attr_reader :noun, :nsfw
+
+    module NSFW
+      WORK_INAPPROPRIATE = /boob|tit|cock|penis|vagina|pussy|dick|ass|fuck|shit|piss|sex|gay|lesbian|chick/
+      def nsfw?
+        true
+      end
+
+      def self.nsfw?(noun)
+        noun =~ WORK_INAPPROPRIATE
+      end
+    end
+
+    module BlackListed
+      BLACKLIST = ["selinaferguson", "pwaring",'eddsowden','shakarshy','nickbrom', 'julietuesley','andrewbrin','dtox','abigailwessel', 'abby', 'angelaparriott', 'elizabethparriott']
+      def file
+        Struct.new(:file, :max_age).new('./copyrightcomplaint.jpg',36000)
+      end
+
+      def self.blacklisted?(noun)
+        noun.downcase!
+        noun.gsub!(/[^\w]*/,'')
+        BLACKLIST.include?(noun)
+      end
+    end
 
     def initialize(noun)
       @noun = noun
@@ -20,32 +42,12 @@ module Actions
 
       instance = new(noun)
 
-      instance.extend(BlackListed) if black_listed?(noun)
-      instance.extend(NSFW)        if nsfw?(noun)
+      instance.extend(BlackListed) if BlackListed.blacklisted?(noun)
+      instance.extend(NSFW)        if NSFW.nsfw?(noun)
 
       instance
     end
 
-    def self.black_listed?(noun)
-      noun.downcase!
-      noun.gsub!(/[^\w]*/,'')
-      BLACKLIST.include?(noun)
-    end
-
-     def self.nsfw?(noun)
-       noun =~ WORK_INAPPROPRIATE
-     end
   end
 
-  module NSFW
-    def nsfw?
-      true
-    end
-  end
-
-  module BlackListed
-    def file
-      Struct.new(:file, :max_age).new('./copyrightcomplaint.jpg',36000)
-    end
-  end
 end
